6b96ae983b697bc49b5b1927d8dfda24b231c1fc
ReynardtDeminey <reynardtdev@gmail.com>
2020-05-18 10:06:08 +0200

    Committing changes

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "Hello from my web app!")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello from my web app!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch contact us at <a href=\"abc@gmail.com\">abc@gmail.com</a>")
+	} else {
+		fmt.Fprint(w, "Sorry, but this page could not be found")
+	}
+
 }
 
 func main() {
